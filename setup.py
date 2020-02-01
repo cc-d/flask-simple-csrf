8e9d7ff593319608d826a96a8713c88962bc9fd8
root <ccarterdev@gmail.com>
2020-02-01 21:09:16 +0000

    setup config

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import os
+from setuptools import setup, find_packages
+
+LOCATION = os.path.dirname(os.path.realpath(__file__)) + '/'
+
+with open(LOCATION + 'README.md', 'r') as r:
+    README = r.read()
+
+setup(
+    name='flask-simple-csrf',
+    version='1.0.0',
+    description='Extremely simple, "Good Enough" csrf implemention for flask forms.',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://github.com/cc-d/flask-simple-csrf',
+    author='Cary Carter',
+    author_email='ccarterdev@gmail.com',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.7.3',
+    ],
+    packages=find_packages(exclude=('tests',)),
+    include_package_data=True,
+    install_requires=['Werkzeug==0.16.0']
+)
