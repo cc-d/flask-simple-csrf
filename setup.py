0aa8c071e244fc24e3355e08e0171ace871cc0ac
root <ccarterdev@gmail.com>
2020-02-02 02:17:20 +0000

    removed build files

--- setup.py
+++ setup.py
@@ -18,8 +18,7 @@ setup(
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.7.3',
+        'Programming Language :: Python :: 3.7'
     ],
     packages=find_packages(exclude=('tests',)),
     include_package_data=True,
